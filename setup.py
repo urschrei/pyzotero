5b628291a1578228a19b97cf2b299c30483bd90c
Stephan Hügel <urschrei@gmail.com>
2011-03-05 13:06:22 +0000

    Adding feedparser to setup requirements

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ Created by Stephan Hügel on 2011-03-04
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-import pyzotero.zotero as z
 
 setup(
     name = 'Pyzotero',
-    version = z.__version__,
+    version = '0.3',
     description = 'Python wrapper for the Zotero API',
     author = 'Stephan Hügel',
     author_email = 'hugels@tcd.ie',
     license = 'GNU GPL Version 3',
     url = 'https://github.com/urschrei/pyzotero',
     packages = find_packages(),
+    install_requires = ['feedparser >= 5.0.1'],
 )
