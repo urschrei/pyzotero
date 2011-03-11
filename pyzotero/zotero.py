eed0ae613bab1f87d1e0770346c9ab47d5b204fb
Stephan Hügel <urschrei@gmail.com>
2011-03-11 21:31:44 +0000

    Don't blow away url_params until just before they're superseded

--- pyzotero/zotero.py
+++ pyzotero/zotero.py
@@ -121,6 +121,8 @@ class Zotero(object):
         """ Set request parameters. Will always add the user ID if it hasn't
             been specifically set by an API call
         """
+        # blow away existing URL params
+        self.url_params = None
         try:
             query = urllib.quote(query_string.format(u = self.user_id))
         except KeyError, err:
@@ -130,7 +132,6 @@ class Zotero(object):
         if not self.url_params:
             self.add_parameters()
         query = '%s?%s' % (query, self.url_params)
-        self.url_params = None
         return query
 
     @retrieve('self.items_data')
