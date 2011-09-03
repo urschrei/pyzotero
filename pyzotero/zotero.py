6b5aa25994875b1ae2afee7a6b345d51fa4ca808
Stephan Hügel <urschrei@gmail.com>
2011-09-03 15:14:04 +0100

    Remember URL parameters when using get_subset()

--- pyzotero/zotero.py
+++ pyzotero/zotero.py
@@ -433,9 +433,14 @@ class Zotero(object):
         if len(subset) > 50:
             raise ze.TooManyItems, \
                     "You may only retrieve 50 items per call"
+        # remember any url parameters that have been set
+        params = self.url_params
         retr = []
         for itm in subset:
             retr.extend(self.item(itm))
+            self.url_params = params
+        # clean up URL params when we're finished
+        self.url_params = None
         return retr
 
     # The following methods process data returned by Read API calls
