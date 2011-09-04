51951115e88c2763454b5538429a357dd6a66734
Stephan Hügel <urschrei@gmail.com>
2011-09-05 00:23:46 +0100

    Item creation JSON should have an "items": field

--- pyzotero/tests.py
+++ pyzotero/tests.py
@@ -586,6 +586,7 @@ class ZoteroTests(unittest.TestCase):
             _ = zot.create_items(ls)
         exc = str(e.exception)
         # this test is a kludge; we're checking the POST data in the 403 response
+        self.assertIn("\"items\":", exc)
         self.assertIn("journalArticle", exc)
         self.assertNotIn("KEYABC123", exc)
         self.assertNotIn("TAGABC123", exc)
