56563716299f36966baadc6a9b7bc747387f9138
Stephan Hügel <urschrei@gmail.com>
2012-01-13 16:35:26 +0000

    Never attempt to Base64 decode JSON content returned by the server

--- pyzotero/zotero.py
+++ pyzotero/zotero.py
@@ -52,7 +52,7 @@ def ib64_patched(self, attrsD, contentparams):
     """ Patch isBase64 to prevent Base64 encoding of JSON content
     """
     if attrsD.get('mode', '') == 'base64':
-        return 1
+        return 0
     if self.contentparams['type'].startswith('text/'):
         return 0
     if self.contentparams['type'].endswith('+xml'):
@@ -61,7 +61,7 @@ def ib64_patched(self, attrsD, contentparams):
         return 0
     if self.contentparams['type'].endswith('/json'):
         return 0
-    return 1
+    return 0
 
 
 # Override feedparser's buggy isBase64 method until they fix it
