e9004bb07ae5799a87fe72f3b3fa882a6fe14d96
Stephan Hügel <urschrei@gmail.com>
2013-03-05 00:47:31 +0000

    Bumping feedparser version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=find_packages(),
-    install_requires=['feedparser >= 5.0.1', 'pytz'],
+    install_requires=['feedparser == 5.1.3', 'pytz'],
     extras_require={
         'ordereddict': ['ordereddict==1.1']
     },
