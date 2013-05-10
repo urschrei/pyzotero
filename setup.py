1439fbc45deba4c5657789f2aa54f590884291ad
Stephan Hügel <urschrei@gmail.com>
2013-05-10 21:03:37 +0100

    Add Requests to setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=find_packages(),
-    install_requires=['feedparser == 5.1.3', 'pytz'],
+    install_requires=['feedparser == 5.1.3', 'pytz', 'requests'],
     extras_require={
         'ordereddict': ['ordereddict==1.1']
     },
