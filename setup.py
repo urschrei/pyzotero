e7139b65d4d67e341b55950a725b041cbea752e0
Stephan Hügel <urschrei@gmail.com>
2014-10-17 17:36:07 +0100

    Fixing broken Travis build again

--- setup.py
+++ setup.py
@@ -8,15 +8,12 @@ Created by Stephan Hügel on 2011-03-04
 import sys
 from setuptools import setup, find_packages
 
-sys.path.insert(1, "..")
-from pyzotero.zotero import __version__ as version
-from pyzotero.zotero import __author__ as author
 
 setup(
     name='Pyzotero',
-    version=version,
+    version='1.0.1',
     description='Python wrapper for the Zotero API',
-    author=author,
+    author='Stephan Hügel',
     author_email='urschrei@gmail.com',
     license='GNU GPL Version 3',
     url='https://github.com/urschrei/pyzotero',
