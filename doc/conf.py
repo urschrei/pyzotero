20e2fca8827b29345a5a1d3269c5f427337f3681
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2015-02-13 12:14:28 +0000

    Remove unused os import in doc conf

--- doc/conf.py
+++ doc/conf.py
@@ -11,7 +11,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
 from datetime import date
 sys.path.insert(1, "..")
 from pyzotero import zotero as zot
