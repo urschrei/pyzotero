8d7e2dc5089a4a14c0ad631148c423f80d9360bc
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2015-02-19 11:57:49 +0000

    Parse version from module in setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,30 @@ setup.py
 
 Created by Stephan Hügel on 2011-03-04
 """
-import sys
+import os
+import re
+import io
 from setuptools import setup, find_packages
-version = '1.1.4'
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]",
+        version_file,
+        re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+version=find_version("pyzotero/zotero.py")
 
 setup(
     name='Pyzotero',
