fec69e472f31ac7e2b09b18c82136b824038212e
Stephan Hügel <urschrei@gmail.com>
2016-07-01 12:50:02 +0200

    Version bump

--- pyzotero/zotero.py
+++ pyzotero/zotero.py
@@ -33,7 +33,7 @@ THE SOFTWARE.
 from __future__ import unicode_literals
 
 __author__ = u'Stephan Hügel'
-__version__ = '1.1.15'
+__version__ = '1.1.16'
 __api_version__ = '3'
 
 # Python 3 compatibility faffing
