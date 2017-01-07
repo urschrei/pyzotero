30e32eb613b4e56c80704e23bf3799c0e73e77c3
Stephan Hügel <urschrei@gmail.com>
2017-01-07 15:22:44 +0000

    Add 3.6 to setup info so it appears on PyPI

--- setup.py
+++ setup.py
@@ -66,5 +66,5 @@ A Python wrapper for the Zotero Server v3 API
 Provides methods for accessing Zotero Server API v3.
 For full documentation see http://pyzotero.readthedocs.org/en/latest/
 
-This version requires Python 2.7.x / 3.4.x / 3.5.x"""
+This version requires Python 2.7.x / 3.4.x / 3.5.x / 3.6.x"""
 )
