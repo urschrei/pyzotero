e21d6d88f49dbdeb2dfb96e68f174ba587eaa27a
Stephan Hügel <urschrei@gmail.com>
2017-06-06 15:24:26 +0100

    Add pre-deploy version match
    [ci skip]

---
+++ pre-deploy.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+pre-deploy.py
+
+Created by Stephan Hügel on 2017-06-06
+
+A simple check to ensure that the tag version and the library version coincide
+Intended to be called before a Wheel is written using "upload"
+"""
+
+import os
+import sys
+import subprocess
+import re
+import io
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]",
+        version_file,
+        re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+def check():
+    git_version = subprocess.check_output(
+        ["git", "describe", "--abbrev=0", "--tags"]
+    ).strip()
+    library_version = unicode("v" + find_version("pyzotero/zotero.py")).strip()
+    return library_version == git_version
+
+if __name__ == '__main__':
+    if check():
+        sys.exit(1)
+    else:
+        sys.exit(0)
