48b3f4694c6fe86f21cc4c8f77f5a2165707b8c6
Stephan Hügel <shugel@tcd.ie>
2022-01-05 18:39:23 +0000

    Process the retry-after header when a 429 response is received

--- pyzotero/zotero.py
+++ pyzotero/zotero.py
@@ -1610,6 +1610,9 @@ def error_handler(zot, req):
             # try to get backoff duration
             delay = req.headers.get("backoff")
             if not delay:
+                # try to get a retry-after duration instead
+                delay = req.headers.get("retry-after")
+            if not delay:
                 raise ze.TooManyRetries(
                     "You are being rate-limited and no backoff duration has been received from the server. Try again later"
                 )
