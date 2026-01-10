1cc9e4e2c2998820360c449faafdd1c01716d768
Stephan Hügel <shugel@tcd.ie>
2026-01-10 20:49:34 +0000

    Use dict comprehension instead of dict() with list comprehension

--- src/pyzotero/_client.py
+++ src/pyzotero/_client.py
@@ -224,13 +224,9 @@ class Zotero:
         # this item's been retrieved from the API, we only need the 'data' entry
         if to_clean.keys() == ["links", "library", "version", "meta", "key", "data"]:
             to_clean = to_clean["data"]
-        return dict(
-            [
-                [k, v]
-                for k, v in list(to_clean.items())
-                if (k in allow or k not in self.temp_keys)
-            ],
-        )
+        return {
+            k: v for k, v in to_clean.items() if k in allow or k not in self.temp_keys
+        }
 
     def _retrieve_data(self, request: str | None = None, params=None):
         """Retrieve Zotero items via the API.
